8882c3059a4829407c1c17cb7043a7aa0ce0c829
JoseMFT <jomaft03@gmail.com>
2022-10-14 11:49:03 +0100

    14/10/2022 - 11:48
    Sí XD

--- JuegoPropio_1/Assets/Scripts/Jugador.cs
+++ JuegoPropio_1/Assets/Scripts/Jugador.cs
@@ -3,30 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Jugador: MonoBehaviour {
-    float timeJump;
     Vector2 movimiento;
     bool puedeSaltar = true;
-    bool saltando = false;
     const float kVelocidad = 15f;
-    void Start () {
-    }
-
-
     void Update () {
         movimiento.x = Input.GetAxis ("Horizontal") * Time.deltaTime * kVelocidad;
-        if (timeJump == 1.5f)
-            puedeSaltar = true;
-        if (saltando == true) {
-            movimiento.y = movimiento.y + kVelocidad * Time.deltaTime;
-        }
-        if (Input.GetKey ("space")) {
-            for (timeJump = 1.5f; timeJump <= 0f; timeJump = timeJump - Time.deltaTime) {
-                puedeSaltar = false;
-                saltando = true;
+        if (/*puedeSaltar == true &&*/ Input.GetKey ("space")) {
+            //saltando ();
+            for (float i = 1.5f; i <= 0f; i -= Time.deltaTime) {
+                movimiento.y += kVelocidad * Time.deltaTime;
             }
         }
     }
+    /*private void saltando () {
+        for (float i = 1.5f; i <= 0f; i -= Time.deltaTime) {
+            movimiento.y += kVelocidad * Time.deltaTime;
+            puedeSaltar = false;
+        }
+        puedeSaltar = true;
+    }*/
     private void FixedUpdate () {
         transform.Translate (movimiento);
+
     }
 }
