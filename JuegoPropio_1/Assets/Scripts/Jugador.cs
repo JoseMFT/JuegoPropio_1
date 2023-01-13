bba9118c431917b3deed105483328638f653b1c4
JoseMFT <jomaft03@gmail.com>
2023-01-13 11:47:40 +0000

    13/01/2023 - 11:47
    Mejoras mecánicas de movimiento

--- JuegoPropio_1/Assets/Scripts/Jugador.cs
+++ JuegoPropio_1/Assets/Scripts/Jugador.cs
@@ -3,27 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Jugador: MonoBehaviour {
-    Vector2 movimiento;
-    bool puedeSaltar = true;
-    const float kVelocidad = 15f;
+    Vector3 playerPos, prevPos, ogSize;
+    public bool canJump, rescale = true, canDash = true;
+    float speed = 10f, jumpForce = 7f;// coolDown = 5f;
+    public int jumpCount = 0;
+
+    Rigidbody2D rigidbodyCharacter;
+
+    private void Awake () {
+        ogSize = gameObject.transform.localScale;
+        rigidbodyCharacter = gameObject.GetComponent<Rigidbody2D> ();
+    }
     void Update () {
-        movimiento.x = Input.GetAxis ("Horizontal") * Time.deltaTime * kVelocidad;
-        if (/*puedeSaltar == true &&*/ Input.GetKey ("space")) {
-            //saltando ();
-            for (float i = 1.5f; i <= 0f; i -= Time.deltaTime) {
-                movimiento.y += kVelocidad * Time.deltaTime;
+        prevPos = playerPos;
+        playerPos = gameObject.transform.position;
+
+        if (canJump == true && Input.GetKey ("space")) {
+            jumpCount++;
+            canJump = false;
+            rigidbodyCharacter.AddForce (Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey ("d")) {
+            gameObject.transform.position += Vector3.right * Time.deltaTime * speed;
+            if (canDash == true) {
+                if (Input.GetKey ("left shift")) {
+                    canDash = false;
+                    DashCD ();
+                    rigidbodyCharacter.AddForce (Vector3.right * speed * 2f, ForceMode2D.Impulse);
+                }
+            }
+        } else if (Input.GetKey ("a")) {
+            gameObject.transform.position += Vector3.left * Time.deltaTime * speed;
+            if (canDash == true) {
+                if (Input.GetKey ("left shift")) {
+                    canDash = false;
+                    DashCD ();
+                    rigidbodyCharacter.AddForce (Vector3.left * speed * 2f, ForceMode2D.Impulse);
+                }
             }
         }
-    }
-    /*private void saltando () {
-        for (float i = 1.5f; i <= 0f; i -= Time.deltaTime) {
-            movimiento.y += kVelocidad * Time.deltaTime;
-            puedeSaltar = false;
-        }
-        puedeSaltar = true;
-    }*/
-    private void FixedUpdate () {
-        transform.Translate (movimiento);
 
+        if (Input.GetKey ("s")) {
+            rescale = false;
+            if (gameObject.transform.localScale.y > ogSize.y * .65f) {
+                gameObject.transform.localScale = new Vector3 (ogSize.x, ogSize.y * .65f, ogSize.z);
+            }
+        } else if (!Input.GetKey ("s")) {
+            if (gameObject.transform.localScale.y != ogSize.y) {
+                rescale = true;
+            }
+        }
+
+        if (prevPos.y > playerPos.y) {
+            if (jumpCount < 2) {
+                canJump = true;
+            }
+        }
+
+        if (rescale == true) {
+            gameObject.transform.localScale = ogSize;
+        }
+
+    }
+
+    public void DashCD () {
+        
+        for (float coolDown = 5f; coolDown <= 0f; coolDown -= Time.deltaTime) {
+
+        }
+    }
+
+
+    private void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.gameObject.tag == "Floor") {
+            jumpCount = 0;
+            canJump = true;
+        }
     }
 }
