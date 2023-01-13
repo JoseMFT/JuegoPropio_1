6f8649a73c20a5312c177bcc067b51002a5e1b6b
JoseMFT <jomaft03@gmail.com>
2023-01-13 14:01:36 +0000

    si

--- JuegoPropio_1/Assets/Scripts/Jugador.cs
+++ JuegoPropio_1/Assets/Scripts/Jugador.cs
@@ -2,20 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Jugador: MonoBehaviour {
-    Vector3 playerPos, prevPos, ogSize;
+    Vector3 playerPos, prevPos, ogSize, dashImagePos;
     public bool canJump, rescale = true, canDash = true;
-    float speed = 10f, jumpForce = 7f;// coolDown = 5f;
+    float speed = 10f, jumpForce = 7f, coolDown = 5f;
     public int jumpCount = 0;
     Rigidbody2D rigidbodyCharacter;
 
     [SerializeField]
-    Image jumpCD, dashCD;
+    GameObject jumpCD, dashCD;
+
+
+
+    [SerializeField]
+    TextMeshProUGUI textoCD;
 
     private void Awake () {
         ogSize = gameObject.transform.localScale;
         rigidbodyCharacter = gameObject.GetComponent<Rigidbody2D> ();
+    }
+    private void Start () {
+        dashImagePos = dashCD.transform.position;
+        textoCD.text = " ";
     }
     void Update () {
         prevPos = playerPos;
@@ -32,16 +42,15 @@ public class Jugador: MonoBehaviour {
             if (canDash == true) {
                 if (Input.GetKey ("left shift")) {
                     canDash = false;
-                    DashCD ();
                     rigidbodyCharacter.AddForce (Vector3.right * speed * 2f, ForceMode2D.Impulse);
                 }
             }
+
         } else if (Input.GetKey ("a")) {
             gameObject.transform.position += Vector3.left * Time.deltaTime * speed;
             if (canDash == true) {
                 if (Input.GetKey ("left shift")) {
                     canDash = false;
-                    DashCD ();
                     rigidbodyCharacter.AddForce (Vector3.left * speed * 2f, ForceMode2D.Impulse);
                 }
             }
@@ -68,15 +77,23 @@ public class Jugador: MonoBehaviour {
             gameObject.transform.localScale = ogSize;
         }
 
-    }
+        if (canDash == false) {
 
-    public void DashCD () {
-
-        for (float coolDown = 5f; coolDown <= 0f; coolDown -= Time.deltaTime) {
-            dashCD.fillAmount = coolDown / 5f;
+            if (coolDown >= 0f) {
+                coolDown -= Time.deltaTime;
+                dashCD.transform.localScale -= new Vector3 (0f, dashCD.transform.localScale.y / coolDown * Time.deltaTime, 0f);
+                dashCD.transform.position = dashImagePos - new Vector3 (0f, dashCD.transform.localScale.y / coolDown * Time.deltaTime, 0f);
+                textoCD.text = coolDown.ToString (".00") + " s";
+            } else {
+                coolDown = 5f;
+                dashCD.transform.localScale = Vector3.one * .5f;
+                dashCD.transform.position = dashImagePos;
+                textoCD.text = " ";
+                canDash = true;
+            }
         }
-    }
 
+    }
 
     private void OnCollisionEnter2D (Collision2D collision) {
         if (collision.gameObject.tag == "Floor") {
