4c1b39ab616d3b613b53f2953654394dcf8bb136
JoseMFT <jomaft03@gmail.com>
2023-01-14 15:09:49 +0000

    14/01/2023 - 15:09
    Terminadas mecáicas de salto y dash.

--- JuegoPropio_1/Assets/Scripts/Jugador.cs
+++ JuegoPropio_1/Assets/Scripts/Jugador.cs
@@ -6,8 +6,8 @@ using TMPro;
 
 public class Jugador: MonoBehaviour {
     Vector3 playerPos, prevPos, ogSize, dashImagePos;
-    public bool rescale = true, canDash = true;
-    float speed = 10f, jumpForce = 10f, dashCoolDown = 5f, jumpCoolDown = 7.5f;
+    public bool rescale = true, canDash = true, onAir = false;
+    float speed = 15f, jumpForce = 6f, dashCoolDown = 5f, doubleJumpCoolDown = 7.5f;
     public int jumpCount = 0, jumpLimit = 2;
     Rigidbody2D rigidbodyCharacter;
 
@@ -33,9 +33,19 @@ public class Jugador: MonoBehaviour {
         prevPos = playerPos;
         playerPos = gameObject.transform.position;
 
-        if (jumpCount < jumpLimit && Input.GetKey ("space")) {
-            jumpCount++;
-            rigidbodyCharacter.AddForce (Vector3.up * jumpForce, ForceMode2D.Impulse);
+        if (jumpCount < jumpLimit) {
+            if (jumpCount == 0) {
+                if (Input.GetKey ("space")) {
+                    jumpCount++;
+                    rigidbodyCharacter.AddForce (Vector3.up * jumpForce, ForceMode2D.Impulse);
+                }
+            } else if (jumpCount == 1) {
+                if (playerPos.y < prevPos.y && Input.GetKey ("space")) {
+                    jumpCD.fillAmount = 0f;
+                    jumpCount++;
+                    rigidbodyCharacter.AddForce (Vector3.up * jumpForce * 6f, ForceMode2D.Impulse);
+                }
+            }
         }
             if (Input.GetKey ("d")) {
                 gameObject.transform.position += Vector3.right * Time.deltaTime * speed;
@@ -53,22 +63,30 @@ public class Jugador: MonoBehaviour {
                 gameObject.transform.position += Vector3.left * Time.deltaTime * speed;
                 if (canDash == true) {
                     if (Input.GetKey ("left shift")) {
-                    dashCD.fillAmount = 0f;
-                    gameObject.GetComponent<TrailRenderer> ().enabled = true;
+                        dashCD.fillAmount = 0f;
+                        gameObject.GetComponent<TrailRenderer> ().enabled = true;
                         canDash = false;
                         gameObject.transform.position += Vector3.left * speed;
-                    speed += speed * .25f;
+                        speed += speed * .25f;
                     }
                 }
             }
 
-        if (Input.GetKey ("s")) {
-            rescale = false;
-            if (gameObject.transform.localScale.y > ogSize.y * .65f) {
-                gameObject.transform.localScale = new Vector3 (ogSize.x, ogSize.y * .65f, ogSize.z);
+        if (onAir == false) {
+            if (Input.GetKey ("s")) {
+                rescale = false;
+                if (gameObject.transform.localScale.y > ogSize.y * .65f) {
+                    gameObject.transform.localScale = new Vector3 (ogSize.x, ogSize.y * .65f, ogSize.z);                    
+                }
+            } else if (!Input.GetKey ("s")) {
+                if (gameObject.transform.localScale.y != ogSize.y) {
+                    rescale = true;
+                }
             }
-        } else if (!Input.GetKey ("s")) {
-            if (gameObject.transform.localScale.y != ogSize.y) {
+        } else if (onAir == true) {
+            if (Input.GetKey ("s")) {
+                rigidbodyCharacter.mass = 6f;
+                gameObject.transform.position += Vector3.down * Time.deltaTime * speed;
                 rescale = true;
             }
         }
@@ -94,30 +112,30 @@ public class Jugador: MonoBehaviour {
         }
 
         if (jumpCount == 2) {
-            jumpLimit--;
+            jumpLimit = 1;
+        } else if (jumpCount != 0) {
+            onAir = true;
         }
 
         if (jumpLimit == 1) {
 
-            jumpCD.fillAmount = 0f;
-
-            if (jumpCoolDown >= 0f) {
-                jumpCoolDown -= Time.deltaTime;
+            if (doubleJumpCoolDown >= 0f) {
+                doubleJumpCoolDown -= Time.deltaTime;
                 jumpCD.fillAmount += 1f / 7.5f * Time.deltaTime;
-                textoCDJump.text = jumpCoolDown.ToString (".00") + " s";
+                textoCDJump.text = doubleJumpCoolDown.ToString (".00") + " s";
             } else {
-                jumpLimit++;
-                jumpCoolDown = 7.5f;
+                jumpLimit = 2;
+                doubleJumpCoolDown = 7.5f;
                 jumpCD.fillAmount = 1f;
                 textoCDJump.text = " ";
             }
         }
-
     }
 
     private void OnCollisionEnter2D (Collision2D collision) {
         if (collision.gameObject.tag == "Floor") {
             jumpCount = 0;
+            onAir = false;
         }
     }
 }
