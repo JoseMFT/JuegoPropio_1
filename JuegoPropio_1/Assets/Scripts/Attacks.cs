684451fc50e6d6f8dd2280de6f79eaa3b377a1b9
JoseMFT <jomaft03@gmail.com>
2023-02-16 09:17:35 +0000

    16/02/2023 - 09:16
    Arreglado sistema de aparición de los puños. Averiguar por que falla el .forward, aunque creo que se debe a las líneas de corrección de la posición en z de los objetos.

--- JuegoPropio_1/Assets/Scripts/Attacks.cs
+++ JuegoPropio_1/Assets/Scripts/Attacks.cs
@@ -6,15 +6,15 @@ using UnityEngine.UI;
 public class Attacks: MonoBehaviour {
     [SerializeField]
     GameObject prefabShot;
-    public float attackCD, attackCDR , attackCDreference;    
+    public float attackCD, attackCDR, attackCDreference;
     public Vector2 destination, mousePos;
     public Image rellenoCD;
 
     void Start () {
-        if (attackCD == null) {
+        if (float.IsNaN (attackCD)) {
             attackCD = .75f;
         }
-        if (attackCDR == null) {
+        if (float.IsNaN (attackCDR)) {
             attackCDR = 1f;
         }
         attackCD = attackCD / attackCDR;
@@ -23,7 +23,7 @@ public class Attacks: MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        
+
         mousePos = Input.mousePosition;
         destination = Camera.main.ScreenToWorldPoint (mousePos);
 
@@ -31,17 +31,35 @@ public class Attacks: MonoBehaviour {
             attackCD -= Time.deltaTime;
         } else {
             if (Input.GetMouseButtonUp (0)) {
-                Instantiate (prefabShot, gameObject.transform.position, Quaternion.identity);
+                Instantiate (prefabShot, gameObject.transform.position, Quaternion.Euler (ReturnSpawnAngle ()));
                 rellenoCD.enabled = true;
                 rellenoCD.fillAmount = 1f;
-                attackCD = attackCDreference/attackCDR;
+                attackCD = attackCDreference / attackCDR;
             }
         }
 
         if (rellenoCD.fillAmount <= 0f && rellenoCD.IsActive () == true) {
             rellenoCD.enabled = false;
-        } else if (rellenoCD.fillAmount > 0f && rellenoCD.IsActive() == true) {
+        } else if (rellenoCD.fillAmount > 0f && rellenoCD.IsActive () == true) {
             rellenoCD.fillAmount -= Time.deltaTime * (attackCDreference / attackCDR);
         }
+    }
+
+    public Vector3 ReturnSpawnAngle () {
+        float orientation = 0f;
+        Vector3 y = Vector3.zero;
+        if (destination.x >= gameObject.transform.position.x) {
+            orientation = 0f;
+        } else {
+            orientation = 180f;
+        }
+        if ((destination.y <= gameObject.transform.position.y + 15f) && (destination.y >= gameObject.transform.position.y - 15f)) {
+            y = new Vector3 (0f, orientation, 0f);
+        } else if (destination.y < gameObject.transform.position.y - 15f) {
+            y = new Vector3 (0f, orientation, -45f);
+        } else if (destination.y > gameObject.transform.position.y + 15f) {
+            y = new Vector3 (0f, orientation, 45f);
+        }
+        return y;
     }
 }
--- JuegoPropio_1/Assets/Scripts/CloneBehaviour.cs
+++ JuegoPropio_1/Assets/Scripts/CloneBehaviour.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CloneBehaviour: MonoBehaviour {
-    float lifetime = 3f, speed = 60f;
-    Vector3 mousePos;
-    GameObject referencePos;
+    public float lifetime = 3f, speed = 60f;
     CanvasGroup fistCanvas;
 
 
     void Start () {
-        mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-        referencePos = new GameObject(" ");
-        referencePos.transform.position = mousePos;
-        gameObject.transform.LookAt (referencePos.transform.position);
-        Destroy (referencePos);
-        fistCanvas = gameObject.GetComponent<CanvasGroup>();
+        fistCanvas = gameObject.GetComponent<CanvasGroup> ();
     }
 
     // Update is called once per frame
@@ -24,6 +17,9 @@ public class CloneBehaviour: MonoBehaviour {
         if (lifetime >= 0f) {
             lifetime -= Time.deltaTime;
             transform.position += transform.forward * Time.deltaTime * speed;
+            if (transform.position.z != 0f) {
+                transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+            }
         } else {
             gameObject.GetComponent<Collider2D> ().enabled = false;
             LeanTween.alphaCanvas (fistCanvas, 0f, 1f).setEaseOutCubic ().setOnComplete (() => {
